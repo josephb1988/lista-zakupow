71da35acbeb9560a5e9ed3e4f819c5707e91444f
josephb1988 <mgogulski@gmail.com>
2017-06-07 20:59:42 +0200

    test

--- hello.go
+++ hello.go
@@ -3,12 +3,13 @@ package main
 import (
     "fmt"
     "net/http"
+    "os"
 )
 
 func main() {
     http.HandleFunc("/", hello)
     fmt.Println("listening...")
-    err := http.ListenAndServe(":5000", nil)
+    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
     if err != nil {
       panic(err)
     }
