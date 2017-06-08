3d4e078e2075c8bdf54688e562166df6d2e6dc6a
josephb1988 <mgogulski@gmail.com>
2017-06-08 21:51:10 +0200

    fix

--- handlers.go
+++ handlers.go
@@ -4,20 +4,19 @@ import (
     "encoding/json"
     "fmt"
     "net/http"
-
+    "strconv"
     "github.com/gorilla/mux"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintln(w, "Hello!")
 }
+var todos = Todos{
+    Todo{Name: "Write presentation"},
+    Todo{Name: "Host meetup"},
+}
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-    todos := Todos{
-        Todo{Name: "Write presentation"},
-        Todo{Name: "Host meetup"},
-    }
-
     if err := json.NewEncoder(w).Encode(todos); err != nil {
         panic(err)
     }
@@ -26,5 +25,20 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 func TodoShow(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
     todoId := vars["todoId"]
+    iTodoID, err:= strconv.Atoi(todoId)
+
+    if ( err != nil ){
+        panic(err)
+    }
+    
     fmt.Fprintln(w, "Todo show:", todoId)
+    fmt.Fprintln(w, "Todo show:", todoId)
+
+    if ( len(todos) > iTodoID ){
+        if err := json.NewEncoder(w).Encode(todos[iTodoID]); err != nil {
+            panic(err)
+        }
+    }else{
+    fmt.Fprintln(w, "No such todo")
+    }
 }
