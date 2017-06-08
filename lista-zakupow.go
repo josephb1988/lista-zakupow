c6aaf73bff14937c7ed1f1bafbf7c835354127ed
josephb1988 <mgogulski@gmail.com>
2017-06-08 21:02:13 +0200

    modify

--- lista-zakupow.go
+++ lista-zakupow.go
@@ -10,28 +10,9 @@ import (
  
 func main() {
 
-	router := mux.NewRouter().StrictSlash(true)
-    router.HandleFunc("/", Index)
-	router.HandleFunc("/todos", TodoIndex)
-    router.HandleFunc("/todos/{todoId}", TodoShow)
-    fmt.Println("listening...")
+	router := NewRouter()
     err := http.ListenAndServe(":"+os.Getenv("PORT"), router)
     if err != nil {
       panic(err)
     }
-}
-
-func Index(res http.ResponseWriter, req *http.Request) {
-    fmt.Fprintf(res, "Hello!" )
-}
-
-
-func TodoIndex(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Todo Index!")
-}
-
-func TodoShow(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    todoId := vars["todoId"]
-    fmt.Fprintln(w, "Todo show:", todoId)
 }
